165fad993f60bbd286bd3eed0d707022af4c52a8
Gabor Szabo <gabor@szabgab.com>
2023-07-12 07:39:15 +0300

    use tiny_http to serve fixed string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use tiny_http::{Response, Server};
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::http("127.0.0.1:3030").expect("Failed to start demo server.");
+    println!("Visit http://localhost:3030 Press Ctrl-C to stop the server.");
+    for req in server.incoming_requests() {
+        req.respond(Response::from_string("Hello World")).unwrap();
+    }
 }
