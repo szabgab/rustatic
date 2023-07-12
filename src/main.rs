214e88693212d4ea22a9711b02f2e47a848fb729
Gabor Szabo <gabor@szabgab.com>
2023-07-12 07:53:02 +0300

    use variables for host and port so we'll be able to configure them from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use tiny_http::{Response, Server};
 
 fn main() {
-    let server = Server::http("127.0.0.1:3030").expect("Failed to start demo server.");
-    println!("Visit http://localhost:3030 Press Ctrl-C to stop the server.");
-    for req in server.incoming_requests() {
-        req.respond(Response::from_string("Hello World")).unwrap();
+    let port = "5000";
+    let host = "127.0.0.1";
+    let server_str = format!("{host}:{port}");
+
+    let server = Server::http(&server_str).expect("Failed to start demo server.");
+    println!("Visit http://{server_str} Press Ctrl-C to stop the server.");
+    for request in server.incoming_requests() {
+        //dbg!(request.method());
+        dbg!(request.url());
+        //dbg!(request.headers());
+        request.respond(Response::from_string("Hello World")).unwrap();
     }
 }
