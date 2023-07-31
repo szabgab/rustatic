3d411f48d24c76e70335b7b518a1d5460dff7019
Gabor Szabo <gabor@szabgab.com>
2023-07-31 13:55:31 +0300

    use the new, but still unreleased version of the markdown crate

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,15 +1,17 @@
-use std::path::Path;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
+use std::io::Read;
+
 
 
 fn main() {
-    let content = markdown::file_to_html(Path::new("../README.md")).unwrap();
+    let source = read_file("../README.md");
+    let content = markdown::to_html(&source);
     //println!("{}", content);
-    let content = content.replace("<h1 ", "<h1 class=\"title\" ");
-    let content = content.replace("<h2 ", "<h2 class=\"title is-4\" ");
-    let content = content.replace("<h3 ", "<h3 class=\"title is-5\" ");
+    let content = content.replace("<h1>", "<h1 class=\"title\">");
+    let content = content.replace("<h2>", "<h2 class=\"title is-4\">");
+    let content = content.replace("<h3>", "<h3 class=\"title is-5\">");
 
     let template = liquid::ParserBuilder::with_stdlib()
         .build()
@@ -30,4 +32,15 @@ fn main() {
 
 }
 
-
+fn read_file(filepath: &str) -> String{
+    match File::open(filepath) {
+        Ok(mut file) => {
+            let mut content = String::new();
+            file.read_to_string(&mut content).unwrap();
+            return content;
+        },
+        Err(error) => {
+            panic!("Error opening file {}: {}", filepath, error);
+        },
+    }
+}
